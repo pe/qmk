13925f157e9695a82559b474bebe271567d36e8c
Philippe Eberli <p.eberli@gmx.ch>
2023-09-25 21:53:11 +0200

    CAPS_LOCK_LED_INDEX is red

--- keymap.c
+++ keymap.c
@@ -80,3 +80,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 }
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 0, 0);
+    } else {
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
+        }
+    }
+    return false;
+}
